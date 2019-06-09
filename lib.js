374dfdba45688c078da08a7d08cf6abd622085ac
Milo <milozhengz@gmail.com>
2019-06-09 17:00:41 +1000

    write pesudocode

---
+++ lib.js
@@ -0,0 +1,18 @@
+// create class object for Pacman with following functions:
+  // place(x, y, f)
+    // make the first valid command
+    // ignore if placed outside grid
+  // move
+    // define how x, y change based on direction
+    // change x or y based on direction - ignore if outside grid
+  // left
+    // change f to previous direction
+  // right
+    // change f to next direction
+  // report
+    // print x, y and f
+
+// ?? "Pacman that is not on the grid can choose the ignore the MOVE, LEFT, RIGHT and REPORT commands."
+// ?? "The first valid command to Pacman is a PLACE command, after that, any sequence of commands may be issued, in any order, including another PLACE command."
+
+
---
+++ pacmanSimulator.js
@@ -0,0 +1,4 @@
+// ask user to place pacman
+// print location placed
+// ask user for command: move, left, place or report
+// print info based on last command
